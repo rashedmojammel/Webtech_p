c3b590878f0d8854df4b240b87336a6bccab0200
rashedmojammel <rashedmojamme56@gmail.com>
2025-11-12 20:37:51 +0600

    Labtask_2

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log("Rashedul alam");
+console.log("American international university bangladesh");
+
+name = "Hello world";
+price = 23.44;
+console.log(name);
+console.log(price);
+
+
+radius = 14;
+console.log(radius);
+
+const student = {
+
+    fullname :"Rashedul alam",
+    cgpa     : 3.70,
+    isPass   : true,
+}
+
+console.log(student["fullname"]);
+
+const product = {
+
+    name   :"Rashedul alam",
+    price  : 3000,
+    isPass : true,
+}
+
+console.log(product);
+console.log(product.name);
+console.log(product.price);
+
+const profile = {
+
+    username  :'@shardakapor',
+    isfollow  : false,
+    followers : 3400,
+
+}
+
+console.log(profile);
+console.log(typeof profile["followers"]);
+
+const student1 = {
+
+    username  :'@shardakapor',
+    isfollow  : true,
+    followers : 45000,
+
+}
+console.log(student1);
+
+
+
+
+
+
+
